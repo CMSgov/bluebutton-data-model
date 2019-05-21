5e713e9a1555ef59c224243124af9ed52bcf0ae0
Jacob Geer <jacob.geer@gdit.com>
2019-05-21 10:07:35 -0500

    Forgot to rename a field
    BLUEBUTTON-865

--- bluebutton-data-model-rif/src/main/resources/db/migration/V15__Add_Inner_Join_Relationships.sql
+++ bluebutton-data-model-rif/src/main/resources/db/migration/V15__Add_Inner_Join_Relationships.sql
@@ -9,17 +9,17 @@
 alter table "Beneficiaries" add column "beneficiaryHistories" varchar(255)
 alter table "Beneficiaries" add column "medicareBeneficiaryIdHistories" varchar(255)
 
-alter table "BeneficiariesHistory" add column "parentBeneficiary" varchar(255);
+alter table "BeneficiariesHistory" add column "beneHistoryParentBeneficiary" varchar(255);
 
 alter table "BeneficiariesHistory" 
    add constraint "BeneficiariesHistory_beneHistoryParentBeneficiary_to_Beneficiary" 
    foreign key ("beneHistoryParentBeneficiary") 
    references "Beneficiaries";
    
-alter table "MedicareBeneficiaryIdHistory" add column "parentBeneficiary" varchar(255);
+alter table "MedicareBeneficiaryIdHistory" add column "mbiParentBeneficiary" varchar(255);
    
 alter table "MedicareBeneficiaryIdHistory" 
    add constraint "MedicareBeneficiaryIdHistory_mbiParentBeneficiary_to_Beneficiary" 
-   foreign key ("parentBeneficiary") 
+   foreign key ("mbiParentBeneficiary") 
    references "Beneficiaries";
 
